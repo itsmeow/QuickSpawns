3a06bbbb99a07e17ef3f0903d784c0f7350421df
itsmeow <itsmeow@itsmeow.dev>
2022-04-14 23:21:55 -0500

    Port to 1.18

--- forge/src/main/java/dev/itsmeow/quickspawns/forge/QuickSpawnsModForge.java
+++ forge/src/main/java/dev/itsmeow/quickspawns/forge/QuickSpawnsModForge.java
@@ -6,14 +6,14 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.IExtensionPoint;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fmllegacy.network.FMLNetworkConstants;
+import net.minecraftforge.network.NetworkConstants;
 
 @Mod(QuickSpawnsMod.MOD_ID)
 @Mod.EventBusSubscriber(modid = QuickSpawnsMod.MOD_ID)
 public class QuickSpawnsModForge {
 
     public QuickSpawnsModForge() {
-        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> FMLNetworkConstants.IGNORESERVERONLY, (s, b) -> true));
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (s, b) -> true));
     }
 
     @SubscribeEvent
